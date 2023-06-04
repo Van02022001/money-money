1ec30b961520a2effff63db8c0198e40da78b327
trungnt <trung2kgl@gmail.com>
2023-06-04 13:11:56 +0700

    [Update] web config, login with

--- src/main/java/com/example/moneymoney/config/WebSecurityConfig.java
+++ src/main/java/com/example/moneymoney/config/WebSecurityConfig.java
@@ -42,20 +42,10 @@ public class WebSecurityConfig {
             "api/v1/money-money/accounts/verification/resend",
             "api/v1/money-money/accounts/password-reset",
             "api/v1/money-money/accounts/password-save",
-
-
-
-
-//            "/",
-//            "/error",
-//            "/favicon.ico",
-//            "/**/*.png",
-//            "/**/*.gif",
-//            "/**/*.svg",
-//            "/**/*.jpg",
-//            "/**/*.html",
-//            "/**/*.css",
-//            "/**/*.js",
+            "/error",
+            "/css/**",
+            "/js/**", "/img/**", "/lib/**",
+            "/favicon.ico",
             "/oauth2/**",
 
 
